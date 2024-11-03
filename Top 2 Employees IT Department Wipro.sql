adb3e14a87f59353a6dfc1ed0438c81170cbfd04
Pradeep-Deep14 <123350286+Pradeep-Deep14@users.noreply.github.com>
2024-11-03 16:37:44 +0530

    Add files via upload

---
+++ Top 2 Employees IT Department Wipro.sql
@@ -0,0 +1,42 @@
+CREATE TABLE employees (employee_id INT PRIMARY KEY, employee_name VARCHAR(100), company_name VARCHAR(100), department VARCHAR(50), salary DECIMAL(10,2));
+
+
+INSERT INTO employees VALUES (1, 'Amit Gupta', 'Wipro', 'IT', 90000.00), 
+	(2, 'Neha Reddy', 'Wipro', 'IT', 85000.00),
+	(3, 'Sanjay Kapoor', 'Wipro', 'HR', 60000.00);
+
+
+
+SELECT * FROM EMPLOYEES
+
+/*
+Find the top 2 employees with the highest salaries in the IT department of Wipro.
+*/
+
+--APPROACH 1
+	
+WITH CTE AS
+(SELECT EMPLOYEE_NAME,
+	    COMPANY_NAME,
+	    DEPARTMENT,
+		DENSE_RANK()OVER(PARTITION BY COMPANY_NAME,DEPARTMENT ORDER BY SALARY )AS DRNK
+FROM EMPLOYEES
+) 
+SELECT EMPLOYEE_NAME,
+       COMPANY_NAME,
+       DEPARTMENT
+FROM CTE
+WHERE DRNK <= 2
+AND DEPARTMENT = 'IT'
+AND COMPANY_NAME = 'Wipro'
+
+--APPROACH 2
+	
+SELECT EMPLOYEE_NAME,
+       COMPANY_NAME,
+       DEPARTMENT,
+       SALARY
+FROM EMPLOYEES
+WHERE COMPANY_NAME = 'Wipro' AND DEPARTMENT = 'IT'
+ORDER BY SALARY DESC
+LIMIT 2;
